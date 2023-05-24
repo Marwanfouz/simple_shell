edd89e502cfdabd60d9aef50a943c221eeeec7ea
marwanfouz <marwanfouz76@gmail.com>
2023-05-25 01:55:30 +0300

    lacis

---
+++ _lacis.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ **_lacis - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_lacis(char *s, char c)
+{
+	do
+	{
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
