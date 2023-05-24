5c11009d9fc836296a27418f084fdc3241e1230b
marwanfouz <marwanfouz76@gmail.com>
2023-05-25 02:13:46 +0300

    rbuffer

---
+++ rbuffer.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * rbuffer - reads a buffer
+ * @info: parameter struct
+ * @buf: buffer
+ * @i: size
+ *
+ * Return: r
+ */
+ssize_t rbuffer(info_t *info, char *buf, size_t *i)
+{
+	ssize_t r = 0;
+
+	if (*i)
+		return (0);
+	r = read(info->readfd, buf, rbuffer_SIZE);
+	if (r >= 0)
+		*i = r;
+	return (r);
+}
