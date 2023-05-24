e93e7573eb69123db3d34f8e028a1a2381bd59ca
marwanfouz <marwanfouz76@gmail.com>
2023-05-25 02:00:49 +0300

    du chars

---
+++ du_chars.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * du_chars - duplicates characters
+ * @pathstr: the PATH string
+ * @start: starting index
+ * @stop: stopping index
+ *
+ * Return: pointer to new buffer
+ */
+char *du_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (pathstr[i] != ':')
+			buf[k++] = pathstr[i];
+	buf[k] = 0;
+	return (buf);
+}
